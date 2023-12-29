ed7a2899fecccfd9a596efad510d7bee8138d4da
wangliang <3130187893@qq.com>
2023-12-29 13:01:48 +0800

    初始化Tiny-Spring，实现了BeanFactory 和 BeanDefinition

---
+++ tiny-Spring-01/src/test/java/com/wangliang/springframework/ApiTest.java
@@ -0,0 +1,21 @@
+package com.wangliang.springframework;
+
+import com.wangliang.springframework.bean.UserService;
+import org.junit.Test;
+
+public class ApiTest {
+
+    @Test
+    public void test_BeanFactory(){
+        //1、初始化BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        //2、注入bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService",beanDefinition);
+
+        //3、获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+}
